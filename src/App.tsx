40d6219cf4249ec39f8992066969e3d51700872d
Yogeshperiyasamy <yogeshsri1209@gmail.com>
2025-02-17 19:54:38 +0530

    tried dynamic content

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ import Message from './Message'
 function App() {
  //here we r retruning jsx which looks like html 
  //jsx==javascript xml
-return <div><Message></Message></div>
+ // we can return duynamic content
+ let name=''
+ if(name)return <div><Message></Message></div>
+ else return <h1> hello world</h1>
+
 }
 
 export default App
