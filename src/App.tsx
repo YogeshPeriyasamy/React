167313a03ebbb5e3a1c8b62e51fae04fd16b9eff
Yogeshperiyasamy <yogeshsri1209@gmail.com>
2025-02-18 21:27:23 +0530

    created date and months added using array

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,32 @@
 import './App.css'
 import Message from './Message'
 function App() {
- //here we r retruning jsx which looks like html 
- //jsx==javascript xml
- // we can return duynamic content
- let name=''
- if(name)return <div><Message></Message></div>
- else return <h1> hello world</h1>
-
-}
-
-export default App
+  interface Exp{
+    heading:string;
+    word:string;
+    date:string;
+    month:string;
+    year:string;
+  }
+  let date=new Date(2025,1,1).getDate().toString();
+  let month=new Date(2025,1,1).getMonth().toString();
+  let year=new Date(2025,1,1).getFullYear().toString();
+   let messages:Exp[]=[{heading:"water",word:"cold",date:date,month:month,year:year},{heading:"fire",word:"hot",date:date,month:month,year:year}];
+    return (
+      <div>
+        <div> 
+          {
+            messages.map((mess)=>{
+              return(
+              <Message heading={mess.heading} word={mess.word} date={mess.date} month={mess.month} year={mess.year} />
+              )
+            })
+          }
+ 
+        </div>
+      </div>
+    );
+  }
+  
+  export default App;
+  
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,13 +1,26 @@
-export default function Message()
+interface MessageType{
+    heading:string;
+    word:string;
+    date:string;
+    month:string;
+    year:string;
+}
+export default function Message({heading,word,date,month,year}:MessageType)
+
 {
     return (
     // to return multiple elements enclose them inside div
     <div>
     <div>
-        <h1>Hello React!</h1>
+        <h1>{heading}</h1>
     </div>
     <div>
-        <p>It's my first try </p>
+        <p>{word}</p>
+    </div>
+    <div>
+    <p>{date}</p>
+    <p>{month}</p>
+    <p>{year}</p>
     </div>
     </div>
     )
