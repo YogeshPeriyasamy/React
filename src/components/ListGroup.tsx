543e7f41cd63786f711dcc56f675cf48f7dabdd2
Yogeshperiyasamy <yogeshsri1209@gmail.com>
2025-02-27 10:49:52 +0530

    added useState

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,38 @@
+import { useState } from "react";
+
 function ListGroup() {
+    let initialLists = ["paris", "germany", "usa", "london", "tamilnadu"];
+    
+    // State to track list items
+    const [lists, setLists] = useState(initialLists);
+
+    const handleClick = (index: number) => {
+        // Create a new array with updated text at the clicked index
+        const updatedLists = lists.map((item, i) => 
+            i === index ? `Clicked ${item}` : item
+        );
+
+        // Update state
+        setLists(updatedLists);
+    };
+
     return (
-        <ul className="list-group">
-            <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-            <li className="list-group-item">A fourth item</li>
-            <li className="list-group-item">And a fifth one</li>
-        </ul>
-    )
+        <>
+            <h1>All Places</h1>
+            {lists.length === 0 ? <p>No places found</p> : null}
+            <ul className="list-group">
+                {lists.map((item, index) => (
+                    <li 
+                        className="places" 
+                        key={index} 
+                        onClick={() => handleClick(index)}
+                    >
+                        {item}
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 }
+
 export default ListGroup;
